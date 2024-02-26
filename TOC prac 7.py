9c54e2b5ff343fb1363fbb080d46611c9ad20eba
Prostorm19 <114726917+Prostorm19@users.noreply.github.com>
2024-02-26 18:16:34 +0530

    Update TOC prac 7.py

--- TOC prac 7.py
+++ TOC prac 7.py
@@ -1,6 +1,6 @@
 #Design a program for creating a machine which accepts string having equal no. of 1’s and 0's
 
-def equal_ones_and_zeros(s):
+ef equal_ones_and_zeros(s):
     state = 'start'
     count_ones = 0
     count_zeros = 0
@@ -17,15 +17,16 @@ def equal_ones_and_zeros(s):
             if char == '1':
                 count_ones += 1
             elif char == '0':
+                state = 'counting_zeros'  # Fix: Change the state to 'counting_zeros'
                 count_zeros += 1
         elif state == 'counting_zeros':
             if char == '0':
                 count_zeros += 1
             elif char == '1':
+                state = 'counting_ones'  # Fix: Change the state to 'counting_ones'
                 count_ones += 1
 
     return count_ones == count_zeros
-
 # Test cases
 print(equal_ones_and_zeros('0101'))  # True
 print(equal_ones_and_zeros('1100'))  # True
